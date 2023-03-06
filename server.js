8731105ce474967a0656d584ef384771932eab87
trista_lea <tristan_lea@outlook.com>
2023-03-05 20:52:49 -0800

    added readFile, writeFile, addNote, all to server.js, updated routes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
+var fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 //const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -11,19 +14,45 @@ app.use(express.json()) // for parsing application/json
 
 // GET Route for homepage
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for feedback page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // Wildcard route to direct users to a 404 page
-app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
+app.get('/api/notes', (req, res) => {
+    let data = readFile();
+    res.json(data.notes);
+}
 );
 
 app.post('/api/notes', (req, res) => 
+    addNote(req.body),
     res.json(req.body)
 );
+
+//making a function to
+function writeFile(data)
+{
+    return fs.writeFileSync("./db/db.json", JSON.stringify(data)); 
+    //writes the file, converts data to a string
+}
+
+function readFile()
+{
+    //add try catch for null error, if null return an obj with id field, like +1 to skip or somehting
+    let data = fs.readFileSync("./db/db.json", "utf8"); 
+    return JSON.parse(data); // seprated to make it easier to read
+    //reads the file and returns the data as a string
+}
+
+function addNote(note)
+{
+    let data = readFile();
+    note.id = data.nextID++; // unique id made, client never sees it. npm uniqid was to big imo
+    data.notes.push(note);
+    writeFile(data); //saves the updated database
+}
